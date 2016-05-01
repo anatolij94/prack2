3552a97408ac325fa9cef45e2132c991c416ef55
anatolij94 <anatolij94@yandex.ru>
2016-05-01 20:45:01 +0300

    unfinished feature step_GOD

--- step.py
+++ step.py
@@ -1,5 +1,9 @@
 def step_HAR(u_LW,t):
     pass
 def step_GOD(u_GOD,t):
-    pass
+    u_curr=[]
+    u_curr.append(uL(t))
+    for i in range(1,len(u_prev)):
+        u_curr.append((1-q)*u_prev[i]+q*u_prev[i-1])
+    return u_curr
   
